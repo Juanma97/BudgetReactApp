1c3c652f63fd558458e02067637e4310a8154b16
Juanma97 <juanma97@outlook.com>
2020-11-03 09:49:23 +0000

    Add error and form validation

--- src/components/Ask.js
+++ src/components/Ask.js
@@ -1,14 +1,39 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState } from 'react'
+import Error from './Error'
 
 const Ask = () => {
+
+    const [value, saveValue] = useState(0);
+    const [error, saveError] = useState(false);
+
+    const setBudget = e => {
+        saveValue(parseInt(e.target.value, 10));
+    }
+
+    const addBudget = e => {
+        e.preventDefault();
+
+        if(value < 1 || isNaN(value)) {
+            saveError(true);
+            return;
+        }
+
+        saveError(false);
+    }
+
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+
+            {error ? <Error message="El presupuesto es incorrecto" /> : null}
+            <form
+                onSubmit={addBudget}
+            >
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={setBudget}
                 />
 
                 <input 
