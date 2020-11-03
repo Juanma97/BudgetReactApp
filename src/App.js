c04e30edec4e9aea9668f97db36eb1e7901b84e7
Juanma97 <juanma97@outlook.com>
2020-11-03 15:30:42 +0000

    Control budget added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Ask from './components/Ask'
 import Form from './components/Form'
+import Expenses from './components/Expenses'
+import ControlBudget from './components/ControlBudget'
 
 function App() {
 
   const [budget, saveBudget] = useState(0);
   const [rest, saveRest] = useState(0);
-
+  const [expenses, saveExpenses] = useState([]);
+  const [expense, saveExpense] = useState({});
   const[showAsk, updateAsk] = useState(true);
+  const [createExpense, saveCreateExpense] = useState(false);
+
+
+  useEffect(() => {
+    if(createExpense){
+      saveExpenses([...expenses, expense])
+
+      const budgetRest = rest - expense.value;
+      saveRest(budgetRest);
+    }
+
+    saveCreateExpense(false);
+    
+  }, [expense])
 
   return (
     <div className="container">
@@ -23,11 +40,20 @@ function App() {
            (
             <div className="row">
               <div className="one-half column">
-                <Form />
+                <Form
+                  saveExpense={saveExpense}
+                  saveCreateExpense={saveCreateExpense}
+                />
               </div>
   
               <div className="one-half column">
-                
+                <Expenses 
+                  expenses={expenses}
+                />
+                <ControlBudget
+                  budget={budget}
+                  rest={rest}
+                />
               </div>
             </div>) }
         </div>
